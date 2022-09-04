fe123fccf5d56bc57281169c280ef4d71cf0edd6
Harshjain3007 <hjain3007@gmail.com>
2022-09-04 23:19:17 +0530

    final

---
+++ src/models/usermodel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      require: true,
+      enum: ["mr", "mrs", "miss"]
+    },
+    name: {
+      type: String,
+      require: true
+    },
+    phone: {
+      type: String,
+      require: true,
+      unique: true
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      require: true
+    },
+    address: {
+      street: String,
+      city: String,
+      pincode: String
+    },
+  },
+  { timestamps: true })
+module.exports = mongoose.model('user', userSchema)
+
+
+
+
+
+
+
---
+++ src/routes/routes.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const aws= require("aws-sdk")
+const userController = require("../Controllers/userController")
+const bookController = require("../controllers/bookController");
+const reviewController = require("../controllers/reviewController");
+const middleware = require("../middlewares/middleware");
+
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
+})
+//===============>>===============[[[[[[[APIS FOR PROJECT3 Books Management]]]]]]]=============<<==================//
+
+//====================Api to create a user=================================//
+
+router.post("/register", userController.registerUser)
+
+//======================Api to login a user================================//
+
+router.post("/login", userController.loginUser)
+
+//==============================Api to create a book=======================//
+
+router.post("/books", middleware.authentication, bookController.createBook);
+
+//=========================api to get the book data========================//
+
+router.get("/books/:bookId", middleware.authentication, bookController.getBooksById);
+
+//==================Api for updating books by bookId in path params========//
+
+router.put("/books/:bookId",  bookController.updateBook);
+
+//Api for deleting books by bookId in path params
+router.delete("/books/:bookId",middleware.authentication,middleware.authorisation,bookController.deleteBook);
+
+//Api for posting review  by bookId in path params
+router.post("/books/:bookId/review", reviewController.createReview);
+
+//Api for updating review  by bookId and review id in path params
+router.put("/books/:bookId/review/:reviewId", reviewController.reviewUpdate);
+
+//Api for deleting review  by bookId and review id in path params
+router.delete("/books/:bookId/review/:reviewId", reviewController.deleteReview);
+
+router.get("/books", middleware.authentication, bookController.getBooks);
+
+// if api is invalid OR wrong URL
+router.all("/*", function (req, res) {
+  res
+    .status(404)
+    .send({ status: false, msg: "The api you requested is not available" });
+});
+
+module.exports = router;
